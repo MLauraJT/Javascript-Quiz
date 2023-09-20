a9c57dfad410006547f5b1450dbf150493d698e3
MLauraJT <mlaurajt@gmail.com>
2023-09-19 20:43:37 -0400

    Made minor change to index.html, added more JS code, mainly focusing on questions

--- assets/script.js
+++ assets/script.js
@@ -47,8 +47,55 @@ var questions = [
 ];
 
 var questionElement = document.getElementById('question');
-var answerButton = document.getElementById('answers');
+var answerButtons = document.getElementById('answers');
 var nextButton = nextButton = document.getElementById('nextbtn')
 
 var currentQuestionIndex = 0;
 var score = 0;
+
+function startQuiz(){
+    currentQuestionIndex = 0;
+    score = 0;
+    nextButton.innerHTML = "Next";
+    showQuestion();
+}
+
+function showQuestion(){
+    resetState();
+    let currentQuestion = questions[currentQuestionIndex];
+    let questionNo = currentQuestionIndex + 1;
+    questionElement.innerHTML = questionNo + ". " + currentQuestion.question;
+
+    currentQuestion.answers.forEach(answer => {
+        var button = document.createElement("button")
+        button.innerHTML = answer.text;
+        button.classList.add("btn");
+        answerButtons.appendChild(button);
+        if(answer.correct){
+            button.dataset.correct = answer.correct;
+        }
+        button.addEventListener("click", selectAnswer);
+    });
+}
+
+
+
+function resetState(){
+    nextButton.style.display = "none";
+    while(answerButtons.firstChild){
+        answerButtons.removeChild(answerButtons.firstChild);
+    }
+}
+
+function selectAnswer(e){
+    var selectedBtn = e.target;
+    var isCorrect = selectedBtn.dataset.correct === "true";
+    if(isCorrect){
+        selectedBtn.classList.add("correct");
+    }else{
+        selectedBtn.classList.add("incorrect");
+    }
+}
+
+startQuiz();
+
